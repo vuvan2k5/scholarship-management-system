002d43061477eafd3a35fc0b9c8aaf3eedaadfe2
hmai280605 <23070585@vnu.edu.vn>
2026-05-12 07:59:22 +0700

    Add delete functionality for disbursements

---
+++ project structure/admin/disbursements/delete.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../../config/database.php";
+
+$id = $_GET['id'];
+
+$sql = "DELETE FROM disbursements WHERE id = $id";
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: index.php");
+    exit();
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+?>

a7ff65f5542f44ffea0364a95e2683aec0eccf00
vuvan2k5 <23070561@vnu.edu.vn>
2026-05-19 08:06:06 +0700

    update

--- project structure/config/app.php
+++ project structure/config/app.php
@@ -1,5 +1,13 @@
 <?php
 
-define('BASE_URL', 'http://localhost/project%20structure');
+if (!defined('BASE_URL')) {
 
-define('BASE_PATH', dirname(__DIR__));
+    define('BASE_URL', 'http://localhost/project%20structure');
+
+}
+
+if (!defined('BASE_PATH')) {
+
+    define('BASE_PATH', dirname(__DIR__));
+
+}
